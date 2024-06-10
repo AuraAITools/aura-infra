6729d04e838eb0184c58fd82621d9234eac83ec5
Kevin Liu Kai <kevinliusingapore@gmail.com>
2024-06-10 23:14:42 +0800

    config: add eip to ec2 instance

---
+++ terraform/eip.tf
@@ -0,0 +1,5 @@
+resource "aws_eip" "ec2_eip" {
+  instance   = aws_instance.ec2.id
+  domain     = "vpc"
+  depends_on = [aws_internet_gateway.igw]
+}
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.8.5"
 
-cloud {
+  cloud {
     organization = "aura-organisation"
 
     workspaces {
