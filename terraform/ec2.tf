ad698c3fa010a2ba6714906049c930fe08237845
Kevin Liu Kai <kevinliusingapore@gmail.com>
2024-06-12 21:16:18 +0800

    config(devops): add keypair to ec2 for ssh

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "ec2" {
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.webserver_sg.id]
+  key_name = "deployer-kp"
   tags = {
     Name        = "aura"
     Environment = "dev"
