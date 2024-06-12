5b3c609b83eb0e38fe86292b9042c105718fe4a5
Kevin Liu Kai <kevinliusingapore@gmail.com>
2024-06-12 21:36:40 +0800

    feat: add script for downloading docker for ec2

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "ec2" {
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.webserver_sg.id]
   key_name = "deployer-kp"
+  user_data = file("${path.module}/scripts/setup.sh")
   tags = {
     Name        = "aura"
     Environment = "dev"
