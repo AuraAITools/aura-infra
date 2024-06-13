83f94e513fadfe5053509137aa542f877235a3de
Kevin Liu Kai <kevinliusingapore@gmail.com>
2024-06-13 21:17:50 +0800

    config: solve hosted zone error

--- terraform/hostedzone.tf
+++ terraform/hostedzone.tf
@@ -1,16 +1,16 @@
 # prod hosted zone
-resource "aws_route53_zone" "main" {
-  name = "auratest.dev"
-  tags = {
-    Name        = "aura"
-    Environment = "prod"
-  }
-}
+# resource "aws_route53_zone" "main" {
+#   name = "auratest.dev"
+#   tags = {
+#     Name        = "aura"
+#     Environment = "prod"
+#   }
+# }
 
-resource "aws_route53_record" "wildcard" {
-  zone_id = aws_route53_zone.main.zone_id
-  name    = "*.auratest.com"
-  type    = "NS"
-  ttl     = "30"
-  records = aws_route53_zone.main.name_servers
-}
+# resource "aws_route53_record" "wildcard" {
+#   zone_id = aws_route53_zone.main.zone_id
+#   name    = "*.auratest.com"
+#   type    = "NS"
+#   ttl     = "30"
+#   records = aws_route53_zone.main.name_servers
+# }
