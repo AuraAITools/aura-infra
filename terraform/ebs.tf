972c2d5a8676719849f88567aa0ece4cc2678a97
Kevin Liu Kai <kevinliusingapore@gmail.com>
2024-06-14 00:36:11 +0800

    fix: remove ebs volume as iops was not the issue for image pull slow speed

--- terraform/ebs.tf
+++ terraform/ebs.tf
@@ -1,10 +1,10 @@
-resource "aws_ebs_volume" "volume" {
-  availability_zone = "ap-southeast-1a"
-  size              = 4
-}
+# resource "aws_ebs_volume" "volume" {
+#   availability_zone = "ap-southeast-1a"
+#   size              = 4
+# }
 
-resource "aws_volume_attachment" "ebs_att" {
-  device_name = "/dev/sdh"
-  volume_id   = aws_ebs_volume.volume.id
-  instance_id = aws_instance.ec2.id
-}
+# resource "aws_volume_attachment" "ebs_att" {
+#   device_name = "/dev/sdh"
+#   volume_id   = aws_ebs_volume.volume.id
+#   instance_id = aws_instance.ec2.id
+# }
