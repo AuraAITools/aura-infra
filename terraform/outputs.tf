7dbcb4da1b064b25f697dfcd3c5fc901f528d365
kevin-otter-liu <kevinliusingapore@gmail.com>
2024-06-15 01:30:49 +0800

    test

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,10 +1,10 @@
 
 output "secret" {
   value = aws_iam_access_key.ecr_svc_user.secret
-  sensitive = false
+  sensitive = true
 }
 
 output "key_id" {
   value = aws_iam_access_key.ecr_svc_user.id
-  sensitive = false
+  sensitive = true
 }
