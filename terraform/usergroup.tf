4dbfdc4f7e098fbb62b4482fb2b450a2a197f583
kevin-otter-liu <kevinliusingapore@gmail.com>
2024-06-16 17:30:04 +0800

    fix: use iam user instead of iam role

--- terraform/usergroup.tf
+++ terraform/usergroup.tf
@@ -1,10 +1,10 @@
-resource "aws_iam_group" "devops" {
-  name = "devops_group"
+resource "aws_iam_group" "ecr_svc_user_group" {
+  name = "ecr_svc_user_group"
   path = "/users/"
 }
 
-resource "aws_iam_group_policy_attachment" "devops_policy_attachment" {
-  group      = aws_iam_group.devops.name
+resource "aws_iam_group_policy_attachment" "ecr_svc_user_group_policy_attachment" {
+  group      = aws_iam_group.ecr_svc_user_group.name
   policy_arn = data.aws_iam_policy.ecr_push_pull_policy.arn
 }
 
@@ -13,6 +13,6 @@ resource "aws_iam_user_group_membership" "ecr_user_membership" {
   user = aws_iam_user.ecr_user.name
 
   groups = [
-    aws_iam_group.devops.name
+    aws_iam_group.ecr_svc_user_group.name
   ]
 }
