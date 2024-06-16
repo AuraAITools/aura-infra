1d4b1038a1f612807c1da85f988b0160c712665c
kevin-otter-liu <kevinliusingapore@gmail.com>
2024-06-16 13:57:52 +0800

    fix: change string

--- terraform/iamrole.tf
+++ terraform/iamrole.tf
@@ -1,5 +1,4 @@
 resource "aws_iam_role" "ecr_svc_user" {
   name               = "ecr_svc_user"
-  assume_role_policy = data.aws_iam_policy.ecr_push_pull_policy
-
+  assume_role_policy = "${data.aws_iam_policy.ecr_push_pull_policy}"
 }
