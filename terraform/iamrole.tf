3a21972fc13afc8c94ec4f785536bdd340387f03
kevin-otter-liu <kevinliusingapore@gmail.com>
2024-06-16 16:39:06 +0800

    fix: fix iam role

--- terraform/iamrole.tf
+++ terraform/iamrole.tf
@@ -1,4 +1,4 @@
 resource "aws_iam_role" "ecr_svc_user" {
   name               = "ecr_svc_user"
-  assume_role_policy = jsonencode(data.aws_iam_policy.ecr_push_pull_policy.policy)
+  assume_role_policy = data.aws_iam_policy.ecr_push_pull_policy.policy
 }
