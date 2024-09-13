c410adc9b139a20629b247cbcaa55f5867e6c071
Kevin Liu Kai <kevinliusingapore@gmail.com>
2024-09-14 00:34:19 +0800

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ec2" {
   ami                    = "ami-0be48b687295f8bd6"
-  instance_type          = "t2.small"
+  instance_type          = "t2.medium"
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.webserver_sg.id]
   key_name               = "deployer-kp"
